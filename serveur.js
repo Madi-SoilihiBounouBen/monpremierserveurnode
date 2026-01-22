c24d5231393dfb6cf54de93a17c7ca2fe0a491f6
Madi-SoilihiBounouBen <madisoilihi1999@gmail.com>
2026-01-22 16:33:33 +0300

    J'ai relié les serveurs et l'application expressjs

--- serveur.js
+++ serveur.js
@@ -6,11 +6,27 @@
  */ 
 const http = require('http');
 
+// j'importe l'application express app.js
+const app = require('./app');
+
+// Je configure le numéro du port utiliser dans le serveur
+app.set('port', 3000);
+
 //Je crée mon serveur en utilisant le package HTTP
 //req = requet, res = réponse
+
+const server = http.createServer(app);
+/*
 const server = http.createServer((req, res) => {
     res.end("Bonjour, je suis le serveur !");
 });
+*/
 
+// Je récupère la date
+const date = new Date();
 // Je précise l'adresse et le port du serveur
-server.listen(3000);
+server.listen(3000, () => {
+    console.log("Le serveur tourne sur le port 3000.")
+    console.log(date.toLocaleDateString());
+    console.log( date.toLocaleTimeString());
+}); 
